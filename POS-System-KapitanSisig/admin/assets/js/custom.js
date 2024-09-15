4fbcd8262ab43e2c095509ba10eddde801709f5d
evanicarose <110085041+evanicarose@users.noreply.github.com>
2024-09-15 21:57:28 +0800

    Add files via upload

---
+++ POS-System-KapitanSisig/admin/assets/js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    $(document).on('click','.increment',function(){
+
+        var $quantityInput = $(this).closest('.qtyBox').find('.qty');
+        var productId  = $(this).closest('.qtyBox').find('.prodId');
+
+        var currentValue = parseInt()
+    });
+});
