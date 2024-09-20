e65561a725d29324ec2148c5e1628694ac437c76
evanicarose <110085041+evanicarose@users.noreply.github.com>
2024-09-20 23:27:53 +0800

    Add files via upload

---
+++ POS-System-KapitanSisig/admin/assets/js/custom.js
@@ -0,0 +1,201 @@
+$(document).ready(function(){
+
+    alertify.set('notifier','position', 'top-right');
+    $(document).on('click', '.increment', function(){
+
+        var $quantityInput = $(this).closest('.qtyBox').find('.qty');
+        var productId  = $(this).closest('.qtyBox').find('.prodId').val();
+
+        var currentValue = parseInt($quantityInput.val());
+
+        if(!isNaN(currentValue)){
+            var qtyVal = currentValue + 1;
+            $quantityInput.val(qtyVal);
+            quantityIncDec(productId, qtyVal);
+        }
+    });
+
+    $(document).on('click', '.decrement', function(){
+
+        var $quantityInput = $(this).closest('.qtyBox').find('.qty');
+        var productId  = $(this).closest('.qtyBox').find('.prodId').val();
+
+        var currentValue = parseInt($quantityInput.val());
+
+        if(!isNaN(currentValue) && currentValue > 1 ){
+            var qtyVal = currentValue - 1;
+            $quantityInput.val(qtyVal);
+            quantityIncDec(productId, qtyVal);  
+        }
+    });
+
+    function quantityIncDec(prodId, qty){
+        $.ajax({
+            type: "POST",
+            url: "orders-code.php",
+            data: {
+                'productIncDec': true,
+                'product_id': prodId,
+                'quantity': qty
+            },
+            success: function(response){
+                var res = JSON.parse(response);
+                // console.log(res);
+
+                if(res.status == 200){
+                    // window.location.reload();
+                    $('#productArea').load(' #productContent');
+                    alertify.success(res.message);
+                } else {
+                    alertify.error(res.message);
+                }
+            }
+        });
+    }
+
+    //proceed to place order button click
+    $(document).on('click', '.proceedToPlace', function(){
+
+        var cphone = $('#cphone').val();
+        var payment_mode = $('#payment_mode').val();
+    
+        // Validate Payment Method
+        if(payment_mode == ''){
+            swal("Select Payment Method", "Select your payment method", "warning");
+            return false;
+        }
+    
+        // Validate Phone Number
+        if(cphone == '' || !$.isNumeric(cphone)){
+            swal("Enter Phone Number", "Enter valid phone number", "warning");
+            return false;
+        }
+    
+        var data = {
+            'proceedToPlaceBtn': true,
+            'cphone': cphone,
+            'payment_mode': payment_mode,
+        };
+    
+        $.ajax({
+            type: "POST",
+            url: "orders-code.php",
+            data: data,
+            success: function(response){
+                var res;
+                try {
+                    res = JSON.parse(response); // Parse response manually, but it could be redundant
+                } catch(e) {
+                    console.error('Error parsing JSON', e);
+                    return;
+                }
+            
+                if (res.status == 200) {
+                    window.location.href = "order-summary.php";
+                } else if (res.status == 404) {
+                    swal(res.message, res.message, res.status_type, {
+                        buttons: {
+                            catch: {
+                                text: "Add Customer",
+                                value: "catch"
+                            },
+                            cancel: "Cancel"
+                        }
+                    }).then((value) => {
+                        switch (value) {
+                            case "catch":
+                                $('#c_phone').val(cphone);
+                                $('#addCustomerModal').modal('show');
+                                // console.log('Pop the customer add modal');
+                                break;
+                            default:
+                        }
+                    });
+                } else {
+                    swal(res.message, res.message, res.status_type);
+                }
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.error('AJAX Error', textStatus, errorThrown);
+                swal('Error', 'Failed to process the request', 'error');
+            }
+            
+        });
+    });
+
+    //Add customers to customer table
+    $(document).on('click', '.saveCustomer', function() {
+
+        var c_name = $('#c_name').val();
+        var c_phone = $('#c_phone').val();
+
+        if(c_name != '' && c_phone != ''){
+            if($.isNumeric(c_phone)){
+
+                var data = {
+                    'saveCustomerBtn': true,
+                    'name': c_name,
+                    'phone': c_phone,  // Correct value for phone
+                };
+                
+
+                $.ajax({
+                    type: "POST",
+                    url: "orders-code.php",
+                    data: data,
+                    success: function(response){
+                        var res = JSON.parse(response);
+
+                        if(res.status == 200){
+                            swal(res.message, res.message, res.status_type);
+                            $('#addCustomerModal').modal('hide');
+                        }else if(res.status == 422){
+                            swal(res.message, res.message, res.status_type);
+                        }else{
+                            swal(res.message, res.message, res.status_type);
+                        }
+                    }
+                });
+            }else{
+                swal("Enter Valid Phone Number","","warning");
+            }
+        }else{
+            swal("Please fill required fields","","warning");
+        }
+
+    });
+
+    $(document).on('click', '#saveOrder', function() {
+        $.ajax({
+            type: "POST",
+            url: "orders-code.php",
+            data: {
+                'saveOrder': true   
+            },
+            success: function(response){
+                try {
+                    var res = JSON.parse(response);  // Parse JSON only if valid
+                    if(res.status == 200){
+                        swal(res.message, res.message, res.status_type);
+                        $('#orderPlaceSuccessMessage').text(res.message);
+                        $('#orderSuccessModal').modal('show');
+
+                    }else{
+                        swal(res.message, res.message, res.status_type);
+                    }
+                } catch (e) {
+                    console.error("Invalid JSON response:", response);
+                    swal("Error", "Unexpected response from server", "error");
+                }
+            },
+            error: function(xhr, status, error) {
+                console.error("AJAX Error:", error);
+                swal("Error", "Failed to process order", "error");
+            }
+        });
+    });    
+
+
+
+    
+});
---
+++ POS-System-KapitanSisig/admin/includes/sidebar.php
@@ -0,0 +1,126 @@
+<div id="layoutSidenav_nav">
+    <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+        <div class="sb-sidenav-menu">
+            <div class="nav">
+                <!-- Main Section -->
+                <div class="sb-sidenav-menu-heading">Main</div>
+                <a class="nav-link" href="index.html">
+                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                    Dashboard
+                </a>
+                <a class="nav-link" href="order-create.php">
+                    <div class="sb-nav-link-icon"><i class="fas fa-bell"></i></div>
+                    Create Order
+                </a>
+                <a class="nav-link" href="orders.php">
+                    <div class="sb-nav-link-icon"><i class="fas fa-list"></i></div>
+                    Orders
+                </a>
+
+                <!-- Interface Section -->
+                <div class="sb-sidenav-menu-heading">Interface</div>
+
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseCategory" aria-expanded="false" aria-controls="collapseCategory">
+                    <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                    Categories
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="collapseCategory" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">   
+                        <a class="nav-link" href="categories-create.php">Add Category</a>
+                        <a class="nav-link" href="categories.php">View Categories</a> 
+                    </nav>
+                </div>
+
+                <!-- Units of Measurement -->
+                <li class="nav-item">
+                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseUnits" aria-expanded="false" aria-controls="collapseUnits">
+                            <i class="fas fa-balance-scale"></i>
+                            <span>Units of Measurement</span>
+                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                        </a>
+                        <div class="collapse" id="collapseUnits" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                            <nav class="sb-sidenav-menu-nested nav">
+                                <a class="nav-link" href="units-create.php">Add Unit</a>
+                                <a class="nav-link" href="units.php">View Units</a>
+                            </nav>
+                        </div>
+                </li>
+
+            
+
+                <!-- Manage Staff Section -->
+                <div class="sb-sidenav-menu-heading">Manage Staff</div>
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseAdmins" aria-expanded="false" aria-controls="collapseAdmins">
+                    <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
+                    Admins/Staff
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="collapseAdmins" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a class="nav-link" href="admins-create.php">Add Admin</a>
+                        <a class="nav-link" href="admins.php">View Admin</a>
+                    </nav>
+                </div>
+
+                <!-- Manage Customer Section -->
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseCustomer" aria-expanded="false" aria-controls="collapseCustomer">
+                    <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
+                    Customer
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="collapseCustomer" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a class="nav-link" href="customers-create.php">Add Customer</a>
+                        <a class="nav-link" href="customers.php">View Customer</a>
+                    </nav>
+                </div>
+
+                <!-- Suppliers Section -->
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseSuppliers" aria-expanded="false" aria-controls="collapseSuppliers">
+                    <div class="sb-nav-link-icon"><i class="fas fa-truck"></i></div>
+                    Suppliers
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="collapseSuppliers" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a class="nav-link" href="suppliers-create.php">Add Suppliers</a>
+                        <a class="nav-link" href="suppliers.php">View Suppliers</a>
+                    </nav>
+                </div>
+
+                <!-- Products Section -->
+                <div class="sb-sidenav-menu-heading">Manage Menu Products</div>
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseProducts" aria-expanded="false" aria-controls="collapseProducts">
+                    <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                    Menu Products
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="collapseProducts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a class="nav-link" href="products.php">View Menu Products</a>
+                        <a class="nav-link" href="products-create.php">Add Menu Product</a>
+                    </nav>
+                </div>
+
+                <!-- Ingredients Section -->
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseIngredients" aria-expanded="false" aria-controls="collapseIngredients">
+                    <div class="sb-nav-link-icon"><i class="fas fa-utensils"></i></div>
+                    Ingredients
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="collapseIngredients" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a class="nav-link" href="ingredients-add.php">Add Ingredients</a>
+                        <a class="nav-link" href="ingredients-view.php">View Ingredients</a>
+                    </nav>
+                </div>
+
+            </div>
+        </div>
+        <div class="sb-sidenav-footer">
+            <div class="small">Logged in as:</div>
+            Start Bootstrap
+        </div>
+    </nav>
+</div>
